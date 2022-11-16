97c61ca4eaa41ed8ab2009f92db7a75d9e629006
Jon Ng <jonathanng15@gmail.com>
2022-11-15 23:34:40 -0800

    Added display name to message view and styled

--- src/ChatComponents/Message.js
+++ src/ChatComponents/Message.js
@@ -5,16 +5,37 @@ import { auth } from '../firebase-config';
 const Message = ({ uid, message, displayName, photoURL }) => {
   const currentUser = auth.currentUser;
   return (
-    <div className={classes.message_container}>
-      <p>{displayName}</p>
-      <img className={classes.display_picture} src={photoURL} />
-      <p
-        className={`${classes.message} ${
-          uid === currentUser.uid ? classes.sent : classes.received
-        }`}
-      >
-        {message}
-      </p>
+    <div
+      className={`${classes.message_container} ${
+        uid === currentUser.uid ? classes.sent : classes.received
+      }`}
+    >
+      <img
+        className={`${classes.display_picture} ${
+          uid === currentUser.uid ? classes.display_picture_sent : ''
+        } `}
+        src={photoURL}
+      />
+      <div className={classes.message_details}>
+        <p
+          className={` ${classes.display_name} ${
+            uid === currentUser.uid
+              ? classes.display_name_sent
+              : classes.display_name_recieved
+          } `}
+        >
+          {displayName}
+        </p>
+        <p
+          className={`${classes.message} ${
+            uid === currentUser.uid
+              ? classes.sent_message
+              : classes.received_message
+          } `}
+        >
+          {message}
+        </p>
+      </div>
     </div>
   );
 };

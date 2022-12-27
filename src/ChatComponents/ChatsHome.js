78272f2896ee8bc5af4e89b8e21f61c0e8b96e30
Jon Ng <jonathanng15@gmail.com>
2022-12-27 01:14:01 -0800

    Added favicon, changed title, and fixed mobile view

--- src/ChatComponents/ChatsHome.js
+++ src/ChatComponents/ChatsHome.js
@@ -2,11 +2,8 @@ import React, { useContext } from 'react';
 import ChatRoom from './ChatRoom';
 import ChatsList from './ChatsList';
 import classes from './ChatsHome.module.css';
-import FirebaseContext from '../store/firebase-context';
 
 const Chats = () => {
-  const firebaseProviderCtx = useContext(FirebaseContext);
-
   return (
     <div className={classes.chats_home_container}>
       <ChatsList />
